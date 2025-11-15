00ef27d5e0afee69e97449fdce2660a0ef54dd97
Gonçalo Moita <gcnma@iscte-iul.pt>
2025-11-15 23:46:04 +0000

    Add comprehensive tests for Compass with 100% coverage

---
+++ src/test/java/iscteiul/ista/battleship/CompassTest.java
@@ -0,0 +1,43 @@
+package iscteiul.ista.battleship;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Testes do enum Compass")
+class CompassTest {
+
+    @Test
+    @DisplayName("Test getDirection() retorna o char correto")
+    void testGetDirection() {
+        assertEquals('n', Compass.NORTH.getDirection());
+        assertEquals('s', Compass.SOUTH.getDirection());
+        assertEquals('e', Compass.EAST.getDirection());
+        assertEquals('o', Compass.WEST.getDirection());
+        assertEquals('u', Compass.UNKNOWN.getDirection());
+    }
+
+    @Test
+    @DisplayName("Test toString() retorna o char correto como String")
+    void testToString() {
+        assertEquals("n", Compass.NORTH.toString());
+        assertEquals("s", Compass.SOUTH.toString());
+        assertEquals("e", Compass.EAST.toString());
+        assertEquals("o", Compass.WEST.toString());
+        assertEquals("u", Compass.UNKNOWN.toString());
+    }
+
+    @Test
+    @DisplayName("Test charToCompass() retorna o Compass correto")
+    void testCharToCompass() {
+        assertEquals(Compass.NORTH, Compass.charToCompass('n'));
+        assertEquals(Compass.SOUTH, Compass.charToCompass('s'));
+        assertEquals(Compass.EAST, Compass.charToCompass('e'));
+        assertEquals(Compass.WEST, Compass.charToCompass('o'));
+        // caracteres desconhecidos retornam UNKNOWN
+        assertEquals(Compass.UNKNOWN, Compass.charToCompass('x'));
+        assertEquals(Compass.UNKNOWN, Compass.charToCompass(' '));
+        assertEquals(Compass.UNKNOWN, Compass.charToCompass('N')); // maiúscula não mapeada
+    }
+}
